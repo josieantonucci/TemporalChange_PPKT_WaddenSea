dc6e5e88ea251d67980658d540b7a84768d56019
josieantonucci <60510989+josieantonucci@users.noreply.github.com>
2023-03-28 23:46:00 +0200

    Phytoplankton analysis

---
+++ R_Scripts/TeporalChangeBiodivBiomass.R
@@ -0,0 +1,66 @@
+
+
+# Temporal change in phytoplankton diversity and FG composition
+
+
+#load packages
+library(vegan)
+library(readr)
+library(plyr)
+library(lattice)
+library(car)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+library(calibrate)
+library(maps)
+library(mapdata)
+library(Hmisc)
+library(psych)
+library(reshape2)
+library(agricolae)
+library(ggExtra)
+library(metafor)
+library(cowplot)
+library(ggridges)
+library(multcomp)
+library(RColorBrewer)
+library(weights)
+library(lubridate)
+library(tidyverse)
+library(nycflights13)
+library(hrbrthemes)
+library(scales)
+library(ggpubr)
+
+library(ggpmisc)
+library(effects)
+library(lme4)
+library(sjPlot)
+library(sjmisc)
+library(sjlabelled)
+library("dplyr")
+
+
+#download the data
+
+### Download 
+setwd("~/Desktop/Job at AWI_HIFMB/nlwkn/my data/Interreg_Josie/InterReg-project/Manuscript/First manuscript_Temporal change")
+
+# data ----------------------
+# Station Map ----------------------
+# Standing Diversity ----------------------
+  #S
+  #ENS
+  #Stats
+# Annual turnover ----------------------
+  #SERa
+  #SERr
+  #Stats
+# Cummulative turnover ----------------------
+  #SERa
+  #SERr
+  #Stats
+# Biomass and functional groups ----------------------
+  #Stats
+
